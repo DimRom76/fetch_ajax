efbe054d2dedea1a7483de88ccd27f793589ee70
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2020-12-13 21:28:17 +0200

    axios

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,20 +1,19 @@
-//import axios from 'axios';
+import axios from 'axios';
 const KEY_API = '19471965-37c13a0c7cc27691c95ac1581';
 const NumberOfPhotos = 12;
 
-// const BASE_URL = `https://pixabay.com`;
-// axios.defaults.baseURL = BASE_URL
-// axios.defaults.headers.common['Authorization'] = KEY_API;
+const headers = { Authorization: `Bearer ${KEY_API}` };
 
-function fetchPhoto(queryString, numberPage, orientation) {
-  // const str = encodeURIComponent(queryString);
-  let url = `https://pixabay.com/api/?key=${KEY_API}&q=${queryString}&image_type=photo&page=${numberPage}&per_page=${NumberOfPhotos}&orientation=${orientation}`;
+async function fetchPhoto(queryString, numberPage, orientation) {
+  const str = encodeURIComponent(queryString);
+  let url = `https://pixabay.com/api/?key=${KEY_API}&q=${str}&image_type=photo&page=${numberPage}&per_page=${NumberOfPhotos}&orientation=${orientation}`;
 
-  console.log(url);
-  return fetch(url)
-    .then(res => res.json())
-    .then(data => data.hits)
-    .catch(err => console.log(err));
+  const response = await axios.get(url);
+  return response.data.hits;
+  // return fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => data.hits)
+  //   .catch(err => console.log(err));
 }
 
 export default fetchPhoto;
